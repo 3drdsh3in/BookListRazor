34c7d4d35e2c5e1d0fc8c257be9033d40776a646
Edward Shen <edwrdshen@gmail.com>
2021-01-26 11:29:33 +1100

    Initial Commit

---
+++ BookListRazor/Pages/BookList/Create.cshtml.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookListRazor.Pages.BookList
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDBContext _db;
+
+        public CreateModel(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+        [BindProperty]
+        public Book Book { get; set; }
+        public void OnGet()
+        {
+
+        }
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                // Add New Book To A Queue.
+                await _db.Book.AddAsync(Book);
+                // Push new changes added to Queue onto DB.
+                await _db.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                return Page();
+            }
+        }
+    }
+}
